6f1425b8ad7005d6d2eb54ca023c8d9395804cd4
noble4real <bert4realove@gmail.com>
2022-07-04 10:11:43 -0700

    hello world 6 correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("size of a char is=%i byte\n", sizeof(char));
-printf("size of an int is=%i byte\n", sizeof(int));
-printf("size of a long int is=%i byte\n", sizeof(long int));
-printf("size of a long long int is=%i byte\n", sizeof(long long int));
-printf("size of a float is=%i byte\n", sizeof(float));
+printf("Size of a char is=%i byte(s)\n", sizeof(char));
+printf("Size of an int is=%i byte(s)\n", sizeof(int));
+printf("Size of a long int is=%i byte(s)\n", sizeof(long int));
+printf("Size of a long long int is=%i byte(s)\n", sizeof(long long int));
+printf("Size of a float is=%i byte(s)\n", sizeof(float));
 return (0);
 }
