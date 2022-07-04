17c9085744d7f17bd8562a07196a8e65d064e4e8
noble4real <bert4realove@gmail.com>
2022-07-04 08:47:48 -0700

    hello world 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float f;
 printf("size of a char is=%i byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int is=%i byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int is=%i byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int is=%i byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a long long int is=%i byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a float is=%i byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
