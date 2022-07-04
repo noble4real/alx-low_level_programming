a2c1570bb32457e0f8c5036614e8dccaf108b0a4
noble4real <bert4realove@gmail.com>
2022-07-04 07:36:09 -0700

    hello world 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>}
 
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the funtion
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
