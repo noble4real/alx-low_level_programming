af3a3b9809ee05510a02b745e0b3fc6792afdea7
noble4real <bert4realove@gmail.com>
2022-07-06 11:53:03 -0700

    new correct 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,6 +5,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -31,7 +32,7 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
