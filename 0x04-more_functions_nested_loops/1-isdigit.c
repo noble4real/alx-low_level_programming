479de7437e51fe4abea464c38599fd60a32aedf6
noble4real <bert4realove@gmail.com>
2022-07-08 14:42:34 -0700

    3 task fix 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isdigit - function that verifies if a character is a digit or not
- * @c: tested charater
+ * @c: tested character
  * Return: returns 1 if it is uppercase , 0 if not
  */
 int _isdigit(int c)
