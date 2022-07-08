eaff95c1576b2df938fdfc183fb364f850fc5c53
noble4real <bert4realove@gmail.com>
2022-07-08 16:01:42 -0700

    fixed task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * _isdigit -> function that verifies if a character is a digit or not
- * @c: tested character
+ * _isdigit - function to check for a digit
+ * @c: an integer argument
  * Return: success
  */
 int _isdigit(int c)
 {
-	
-	reture (c >= '0' && c <= '9');
+		reture (c >= '0' && c <= '9');
 }
