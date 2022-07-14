43a4355f8f3d553bf36cce77b78f54e04d747a69
noble4real <bert4realove@gmail.com>
2022-07-14 14:14:40 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,42 @@
 #include "main.h"
+
 /**
- * rot13 - function that encodes string using rot13
- * @s: string
- * Return: encoded string
+ * rot13 - encodes a string using rot13
+ * @str:the string targeted
+ * Return: return the encoded string
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int x, y;
+int index1, index2;
 
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char ouput[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (x = 0; s[x] != '\0'; x++)
-	{
-		for (y = 0; input[y] != '\0'; y++)
-		{
-			if (s[x] == input[y])
-			{
-				s{x} = output[y];
-				break;
-			}
-		}
-	}
-	return (s);
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
+while (str[++index1])
+{
+for (index2 = 0; index2 < 52; index2++)
+{
+if (str[index1] == alphabet[index2])
+{
+str[index1] = rot13key[index2];
+break;
+}
+}
+}
+return (str);
 }
