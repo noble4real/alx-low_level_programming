bf083062c0709ca9c3e322b9a800010f93b1e36d
noble4real <bert4realove@gmail.com>
2022-07-14 07:08:59 -0700

    task 1 fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 /**
  * _strncpy - c function that copies a string, including the
  * terminating null byte, using at most an inputted number of bytes.
- * If the length of the source string is less than than the maximum byte number,
+ * If the length of the source string is less than the maximum byte number,
  * the remainder of the destination string is filled with null bytes.
  * works identically to the standard library function 'strncpy'.
  * @dest: buffer storing the string copy
