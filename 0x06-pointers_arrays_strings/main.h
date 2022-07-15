2283a0e71e095e138931ecbe932e64b0a76990d3
noble4real <bert4realove@gmail.com>
2022-07-15 08:01:45 -0700

    hottest fixed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,6 +19,7 @@ char *leet(char *);
 int _putchar(char c);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 
 #endif /* MAIN_H */
